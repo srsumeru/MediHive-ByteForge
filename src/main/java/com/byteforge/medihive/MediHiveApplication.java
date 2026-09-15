ee1fdff6a1b6d6a4a602179215ad8b281fdd83eb
Sumeru S R <srsumeru99@gmail.com>
2026-09-15 22:59:12 +0530

    Add Spring Boot application entry point

---
+++ src/main/java/com/byteforge/medihive/MediHiveApplication.java
@@ -0,0 +1,11 @@
+package com.byteforge.medihive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediHiveApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MediHiveApplication.class, args);
+    }
+}
